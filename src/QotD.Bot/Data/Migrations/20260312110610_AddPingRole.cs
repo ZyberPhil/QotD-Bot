13b28516d745d1dd1696406779786a39f8429be3
ZyberPhil <philhempel4@gmail.com>
2026-03-12 12:06:49 +0100

    feat: add PingRoleId column to GuildConfigs and update model snapshot

---
+++ src/QotD.Bot/Data/Migrations/20260312110610_AddPingRole.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QotD.Bot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPingRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "PingRoleId",
+                table: "GuildConfigs",
+                type: "numeric(20,0)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PingRoleId",
+                table: "GuildConfigs");
+        }
+    }
+}
