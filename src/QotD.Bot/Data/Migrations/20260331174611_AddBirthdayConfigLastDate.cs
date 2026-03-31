ca104bf39a8130840a5be44df2e3d59ce95ec3a9
ZyberPhil <philhempel4@gmail.com>
2026-03-31 19:46:47 +0200

    feat: Add Birthday Reminder feature + AddBirthdayConfigLastDate migration

---
+++ src/QotD.Bot/Data/Migrations/20260331174611_AddBirthdayConfigLastDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QotD.Bot.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBirthdayConfigLastDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "LastAnnouncementDate",
+                table: "BirthdayConfigs",
+                type: "date",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastAnnouncementDate",
+                table: "BirthdayConfigs");
+        }
+    }
+}
