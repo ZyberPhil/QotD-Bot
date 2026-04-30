549de69e12f150ad4e0a60bc12af406ed3767cf0
ZyberPhil <philhempel4@gmail.com>
2026-04-30 18:09:21 +0200

    Fix: Register logging event handlers in Discord client DI container

--- src/QotD.Bot/Features/Logging/LoggingModule.cs
+++ src/QotD.Bot/Features/Logging/LoggingModule.cs
@@ -1,8 +1,11 @@
+using DSharpPlus;
 using DSharpPlus.Commands;
+using DSharpPlus.EventArgs;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using QotD.Bot.Core;
 using QotD.Bot.Features.Logging.Commands;
+using QotD.Bot.Features.Logging.Services;
 
 namespace QotD.Bot.Features.Logging;
 
@@ -10,10 +13,25 @@ public sealed class LoggingModule : IBotModule
 {
     public void ConfigureServices(IServiceCollection services, IConfiguration configuration)
     {
+        services.AddSingleton<DiscordLoggingEventHandler>();
+        services.AddSingleton<LogSetupEventHandler>();
+        services.AddSingleton<DiscordBotLogRelay>();
+        services.AddHostedService<DiscordBotLogPump>();
     }
 
     public void ConfigureDiscordServices(IServiceCollection services, IServiceProvider hostProvider)
     {
+        var loggingHandler = hostProvider.GetRequiredService<DiscordLoggingEventHandler>();
+        services.AddSingleton(loggingHandler);
+        services.AddSingleton<IEventHandler<MessageDeletedEventArgs>>(loggingHandler);
+        services.AddSingleton<IEventHandler<MessageUpdatedEventArgs>>(loggingHandler);
+        services.AddSingleton<IEventHandler<GuildMemberAddedEventArgs>>(loggingHandler);
+        services.AddSingleton<IEventHandler<GuildMemberRemovedEventArgs>>(loggingHandler);
+        services.AddSingleton<IEventHandler<VoiceStateUpdatedEventArgs>>(loggingHandler);
+
+        var logSetupHandler = hostProvider.GetRequiredService<LogSetupEventHandler>();
+        services.AddSingleton(logSetupHandler);
+        services.AddSingleton<IEventHandler<ComponentInteractionCreatedEventArgs>>(logSetupHandler);
     }
 
     public void ConfigureCommands(CommandsExtension commands)
